29929d3cc18d6dfbc1cc26ef064688cccec6dc5d
Tom Woodard <woodardt@amazon.com>
2021-02-02 16:10:56 -0800

    minor update

--- bin/onward-blog.ts
+++ bin/onward-blog.ts
@@ -4,7 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { OnwardBlogStack } from '../lib/onward-blog-stack';
 
 const app = new cdk.App();
-new OnwardBlogStack(app, 'OnwardPhotosStack', {
+new OnwardBlogStack(app, 'OnwardBlogStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'us-east-1'
