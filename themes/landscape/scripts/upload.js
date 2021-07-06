e5ba13641e3c60540f6b0d3c5f3089a802023ba4
Tom Woodard <woodardt@amazon.com>
2021-07-06 12:09:33 -0700

    deploy push

--- themes/landscape/scripts/upload.js
+++ themes/landscape/scripts/upload.js
@@ -1,15 +1,22 @@
+const { execSync } = require('child_process');
 
+var options = {
+  usage: 'None',
+  arguments: [],
+  desc: "uploads local changes and syncs to live for deployment in Github"
+}
 
 hexo.extend.console.register('upload', options.desc, options, function(args){
-
+  var stdout;
 
   console.log('Pushing local changes to main (if any)...')
-  let stdout = execSync("git commit -a -m 'deploy push' && git push");
+  stdout = execSync("git commit -a -m 'deploy push' && git push");
   console.log(stdout)
   console.log('Local changes pushed.')
+
   console.log('Pushing Main to Live...')
-  let stdout = execSync("git push origin main:live");
+  stdout = execSync("git push origin main:live")
   console.log(stdout)
-  console.log('Main pushed to Live.'
+  console.log('Main pushed to Live.')
   console.log(args);
 });
