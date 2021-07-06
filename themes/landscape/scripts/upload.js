cf4d4606c9a9bc8a0cfb28161551cb3bda772446
Tom Woodard <woodardt@amazon.com>
2021-07-06 12:04:31 -0700

    minor update

--- themes/landscape/scripts/upload.js
+++ themes/landscape/scripts/upload.js
@@ -1,10 +1,15 @@
 
 
 hexo.extend.console.register('upload', options.desc, options, function(args){
-  console.log('Pushing changes to main (if any)...')
-  let stdout = execSync('');
+
+
+  console.log('Pushing local changes to main (if any)...')
+  let stdout = execSync("git commit -a -m 'deploy push' && git push");
   console.log(stdout)
-  console.log('CDK deployment complete.')
+  console.log('Local changes pushed.')
+  console.log('Pushing Main to Live...')
+  let stdout = execSync("git push origin main:live");
+  console.log(stdout)
+  console.log('Main pushed to Live.'
   console.log(args);
-//tewst
 });
