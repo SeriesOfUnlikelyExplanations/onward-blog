c3f839cf23e9a4122ab8a3d3cfacd668c4cc76b9
Tom Woodard <woodardt@amazon.com>
2021-07-06 08:23:40 -0700

    minor update

--- themes/landscape/scripts/deploy.js
+++ themes/landscape/scripts/deploy.js
@@ -17,8 +17,13 @@ hexo.extend.deployer.register('cdk', function(args) {
     accessKeyId: args.aws_key || process.env.AWS_ACCESS_KEY_ID || process.env.AWS_KEY,
     secretAccessKey: args.aws_secret || process.env.AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET,
   });
-  console.log(this.config.public_dir)
 
+  var ssm = new AWS.SSM();
+  var ssmData = ssm.getParameters({Names: [args.bucket, args.distID]}, (err, data) => {
+    if (err) console.log(err, err.stack); // an error occurred
+    else return data;           // successful response
+  });\
+  console.log(ssmData);
   const bucketName = 'blog.always-onward.com'
 
   function walkSync(currentDirPath, callback) {
@@ -39,7 +44,7 @@ hexo.extend.deployer.register('cdk', function(args) {
       Bucket: bucketName,
       Key: bucketPath,
       Body: fs.readFileSync(filePath),
-      ContentType: mime.getType(filePath),
+      ContentType: mime.getType(filePath)
     };
     s3.putObject(params, function(err, data) {
       if (err) {
@@ -50,42 +55,9 @@ hexo.extend.deployer.register('cdk', function(args) {
     });
   });
   console.log('S3 deployment complete.')
+  console.log('Starting cloudfront invalidation...')
+  var cloudfront = new AWS.CloudFront();
+
+  //~ "invalidate": "aws cloudfront create-invalidation --distribution-id `jq -r '.OnwardBlogStack.distID' output.json` --paths '/*'",
   return
 });
-
-
-//~var config = {
-     //~maxAsyncS3: args.concurrency || 20,
-     //~s3Options: {
-       //~accessKeyId:
-       //~secretAccessKey:
-       //~region: args.region
-     //~}
-   //~};
-
-   //~var client = s3.createClient(config);
-   //~if (!args.bucket || !config.s3Options.accessKeyId || !config.s3Options.secretAccessKey) {
-     //~console.log('You are missing one or more required parameters.');
-     //~return;
-   //~}
-
-   //~var params = {
-     //~localDir: this.config.public_dir,
-     //~deleteRemoved: args.hasOwnProperty('delete_removed') ? Boolean(args.delete_removed): true,
-     //~s3Params: {
-       //~Bucket: args.bucket,
-       //~Prefix: args.prefix
-     //~}
-   //~}
-
-   //~var uploader = client.uploadDir(params);
-   //~this.log.info('Uploading...');
-
-   //~return uploader
-     //~.on('progress', function() {
-       //~//   log.info(uploader.progressAmount + ' / ' + uploader.progressTotal);
-     //~}).on('end', function() {
-       //~this.log.info('Done!');
-     //~}).on('error', function(err) {
-       //~this.log.error(err)
-     //~});
