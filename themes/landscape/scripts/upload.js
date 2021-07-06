ccc7c5657ced9f4cff5de1a72d59b14afc561076
Tom Woodard <woodardt@amazon.com>
2021-07-06 12:11:15 -0700

    deploy push

--- themes/landscape/scripts/upload.js
+++ themes/landscape/scripts/upload.js
@@ -11,11 +11,11 @@ hexo.extend.console.register('upload', options.desc, options, function(args){
   // first push any local changes to main branch
   console.log('Pushing local changes to main (if any)...')
   stdout = execSync("git commit -a -m 'deploy push' && git push");
-  console.log(stdout.string())
+  console.log(stdout)
   console.log('Local changes pushed.')
   // then sync the main branch to the live branch - which will trigger the github workflow
   console.log('Pushing Main to Live...')
   stdout = execSync("git push origin main:live")
-  console.log(stdout.string())
+  console.log(stdout)
   console.log('Main pushed to Live.')
 });
