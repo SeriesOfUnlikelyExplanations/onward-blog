5df69363edb442fdab43579e7f8f407d87491de6
Tom Woodard <woodardt@amazon.com>
2021-07-06 12:02:25 -0700

    deploy push

--- themes/landscape/scripts/upload.js
+++ themes/landscape/scripts/upload.js
@@ -1,6 +1,10 @@
 
 
 hexo.extend.console.register('upload', options.desc, options, function(args){
+  console.log('Pushing changes to main (if any)...')
+  let stdout = execSync('');
+  console.log(stdout)
+  console.log('CDK deployment complete.')
   console.log(args);
 //tewst
 });
